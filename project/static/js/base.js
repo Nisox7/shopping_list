65eb0cd384c9df8a6fe7e3a5081988d446ef838c
nisox7 <nicomuller2704@gmail.com>
2023-06-21 20:53:19 -0300

    Admin database is full responsive, it adapts to the screen. Also the navbar links work with active property

---
+++ project/static/js/base.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var navLinks = document.querySelectorAll('.nav-link');
+    var themeToggle = document.querySelector('.theme-toggle');
+  
+    // Add event listener to each navigation link
+    function addClickListenerToNavLinks() {
+      navLinks.forEach(function(navLink) {
+        navLink.addEventListener('click', function(event) {
+          // Remove active class from all navigation links except the theme toggle
+          if (!this.classList.contains('dropdown-toggle')) {
+            navLinks.forEach(function(link) {
+              link.classList.remove('active');
+            });
+          }
+  
+          // Add active class to the clicked navigation link
+          this.classList.add('active');
+        });
+      });
+    }
+  
+    // Check if the link's href matches the current page URL
+    navLinks.forEach(function(navLink) {
+      if (navLink.href === window.location.href) {
+        navLink.classList.add('active');
+      }
+    });
+  
+    addClickListenerToNavLinks();
+  });
+  
